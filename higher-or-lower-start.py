2ed2bcf89f52a4daa30ee8ff0de4d8e5a6837b04
Bradley Joseph <infinity_ninja1239@macusers-MacBook.local>
2020-07-15 15:17:28 +0100

    i added more text

--- higher-or-lower-start.py
+++ higher-or-lower-start.py
@@ -3,6 +3,6 @@ guessNum = 100
 if yourNum == guessNum :
     print("You got it right!")
 elif yourNum > guessNum :
-    print("Lower than that!")
+    print("Lower than that! Try again!")
 elif yourNum < guessNum:
-    print("Bigger than that!")
+    print("Bigger than that! Try again!")
