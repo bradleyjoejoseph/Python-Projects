0891d2599c8db6f97cf18e18f519685a2527b1ff
Bradley Joseph <infinity_ninja1239@macusers-MacBook.local>
2020-07-15 14:58:06 +0100

    intial commit

---
+++ higher-or-lower-start.py
@@ -0,0 +1,8 @@
+yourNum = 24
+guessNum = 100
+if yourNum == guessNum :
+    print("You got it right!")
+elif yourNum > guessNum :
+    print("Lower than that!")
+elif yourNum < guessNum:
+    print("Bigger than that!")
